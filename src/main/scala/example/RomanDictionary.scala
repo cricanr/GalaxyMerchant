1d4a848ca762807d95ac8517b354b69641908859
Radu Crican <radu.crican@workday.com>
2018-04-16 15:14:40 +0200

    Fixed test

--- src/main/scala/example/RomanDictionary.scala
+++ src/main/scala/example/RomanDictionary.scala
@@ -3,7 +3,7 @@ package example
 class RomanDictionary {
   def translateRomanNumber(romanNumber: String): Int = {
     if (romanNumber.length == 1) romanSymbols(romanNumber) else {
-    for (i <- 0 to romanNumber.length) yield {
+    for (i <- 0 until romanNumber.length-1) yield {
       (romanNumber(i), romanNumber(i + 1)) match {
         case (first, second) if romanSymbols(first.toString) > romanSymbols(second.toString) =>
           romanSymbols(first.toString)
@@ -11,6 +11,7 @@ class RomanDictionary {
           -romanSymbols(first.toString)
         case (first, second) if romanSymbols(first.toString) < romanSymbols(second.toString) && second == romanNumber(romanNumber.length-1) =>
           -romanSymbols(first.toString) + romanSymbols(second.toString)
+        case (first, second) => romanSymbols(first.toString) + romanSymbols(second.toString)
       }}}.sum
   }
 
